72c73273ba4e61b478da33d450a6ebf53b0add8f
2011brunofernandes <rgr.alexandre@tokiomarine.com.br>
2021-12-15 17:13:37 -0300

    foreach

---
+++ forEach.php
@@ -0,0 +1,10 @@
+<?php
+$ingredientes = [
+    'farinha',
+    'ovo'
+]; 
+
+foreach($ingredientes as $ing){
+    echo 'Item: ' .$ing. '<br/>';
+}
+?>
---
+++ forEachArray.php
@@ -0,0 +1,10 @@
+<?php
+$ingredientes = [
+    'farinha',
+    'ovo'
+]; 
+
+foreach($ingredientes as $chave => $ing){
+    echo 'Item: '.$chave.' ' .$ing. '<br/>';
+}
+?>
