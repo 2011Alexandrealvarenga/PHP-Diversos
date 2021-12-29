a4714ae8f4d3e8c3e8817d4fb6249b61d0764e54
2011brunofernandes <rgr.alexandre@tokiomarine.com.br>
2021-12-28 21:25:32 -0300

    key and values

---
+++ diversos/array/keys_values.php
@@ -0,0 +1,14 @@
+<?php 
+$array = [
+    'nome' => 'Ale',
+    'idade' => 90,
+    'empresa' => 'Ale Empresa'
+];
+// colunas a esquerda da array
+// $chaves = array_keys($array);
+// print_r($chaves);
+
+$valores = array_values($array);
+print_r($valores);
+
+?>
