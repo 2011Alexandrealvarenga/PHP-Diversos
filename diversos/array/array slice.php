c424fe734640f71af66f8114fb614b14c648a5b2
2011brunofernandes <rgr.alexandre@tokiomarine.com.br>
2021-12-29 10:11:22 -0300

    array slice

---
+++ diversos/array/array slice.php
@@ -0,0 +1,18 @@
+<?php 
+$array = ['a','b','c','d','e'];
+
+// array slice, corta o array, copia o array em pedaços
+
+// primeiro parametro, array 
+// segundo de onde por posição
+// quantos itens
+
+// 4 itens
+// $retorno = array_slice($array, 0, 4);
+
+// itens 2 a 4
+$retorno = array_slice($array, 2, 4);
+
+
+print_r($retorno);
+?>
