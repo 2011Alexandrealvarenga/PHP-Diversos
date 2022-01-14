6a0bd097ed446fb91d8fdfa0ab4db18df3951c56
2011brunofernandes <rgr.alexandre@tokiomarine.com.br>
2022-01-14 20:31:47 -0300

    CRUD - validacao de email

--- CRUD V2/adicionar_action.php
+++ CRUD V2/adicionar_action.php
@@ -5,6 +5,23 @@ $name = filter_input(INPUT_POST, 'name');
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
 if($name && $email){
+    // traz todos os registros de email
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+    $sql->bindValue(':email',$email);
+    $sql->execute();
+
+    // se NÃO tiver registro entao salva no bd
+    if($sql->rowCount() ===0){
+
+        $sql = $pdo->prepare("INSERT INTO usuarios (nome, email) VALUES(:name, :email)");
+        $sql->bindValue(':name', $name);
+        $sql->bindValue(':email', $email);
+        $sql->execute();
+        header("location: index.php");
+    }else{
+        header("location: index.php");
+        exit;
+    }
 
 }else{
     // se der erro, volta para o arquivo adicionar.php
