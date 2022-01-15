dd3ac4ae537adaeb232920bfc22aaacc1f98eafe
2011brunofernandes <rgr.alexandre@tokiomarine.com.br>
2022-01-15 20:07:30 -0300

    pegar informações de usuario no banco e exibir

---
+++ CRUD/CRUD EXERCICIO 5/config.php
@@ -0,0 +1,8 @@
+<?php 
+$db_name='test';
+$db_host='localhost';
+$user= 'root';
+$pass='';
+
+$pdo = new PDO('mysql:dbname='.$db_name.';host='.$db_host, $user, $pass);
+?>
---
+++ CRUD/CRUD EXERCICIO 5/index.php
@@ -0,0 +1,26 @@
+<?php 
+require 'config.php';
+$lista = [];
+$sql = $pdo->query("SELECT * FROM usuarios");
+
+if($sql->rowCount() > 0){
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
+?>
+<a href="adicionar.php">ADICIONAR USUARIO</a><br>
+<table border="1" width="100%">
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>EMAIL</th>
+        <th>AÇÕES</th>
+    </tr>
+    <?php foreach($lista as $usuarios): ?>
+    <tr>
+        <td><?php echo $usuarios['id']; ?> </td>
+        <td><?php echo $usuarios['nome']; ?> </td>
+        <td><?php echo $usuarios['email']; ?> </td>
+        <td></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
