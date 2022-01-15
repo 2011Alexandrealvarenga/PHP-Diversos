28142f28eb40b3d0c2f6bb3bb6730ba8acd88daf
2011brunofernandes <rgr.alexandre@tokiomarine.com.br>
2022-01-14 21:09:44 -0300

    CRUD - form editar

--- CRUD V2/index.php
+++ CRUD V2/index.php
@@ -1,6 +1,16 @@
 <?php 
 require 'config.php';
+
+$lista = [];
+$sql = $pdo->query("SELECT * FROM usuarios");
+
+// se tiver registro, joga as informações para a variavel lista
+if($sql->rowCount() > 0){
+
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
+
 <a href="adicionar.php">ADICIONAR USUARIO</a>
 
 <table border='1' width="100%">
@@ -10,4 +20,17 @@ require 'config.php';
         <th>EMAIL</th>
         <th>AÇÕES</th>
     </tr>
+    <?php 
+    foreach($lista as $usuario):?>
+        <tr>
+            <td><?php echo $usuario['id']; ?></td>
+            <td><?php echo $usuario['nome']; ?> </td>
+            <td><?php echo $usuario['email']; ?></td>
+            <td>
+                <a href="editar.php?id=<?php echo $usuario['id']; ?>">[ Editar ]</a>
+                <a href="excluir.php?id=<?php echo $usuario['id']; ?>">[ Excluir ]</a>
+
+            </td>
+        </tr>
+    <?php endforeach; ?>
 </table>
