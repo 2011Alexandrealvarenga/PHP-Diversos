19f55584f88cc54a7c67956ae5a5b0d629d145ac
2011brunofernandes <rgr.alexandre@tokiomarine.com.br>
2022-01-16 18:48:51 -0300

    exercicios 3 adicionar

---
+++ CRUD/CRUD EXERCICIO 3/adicionar.php
@@ -0,0 +1,14 @@
+<h1>adicionar usuario</h1>
+
+<form method="POST" action="adicionar_action.php">
+    <label for=""> <br>
+        nome: <br>
+        <input type="text" name="name">        
+    </label>
+    <label for=""> <br>
+        email: <br>
+        <input type="text" name="email">        
+    </label><br>
+    <input type="submit" value="Adicionar">
+    
+</form>
